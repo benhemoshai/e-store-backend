e33baf4e4256e2d9e343a1dffaa79251fe56cb3f
Shai <benhemoshai@gmail.com>
2024-11-24 14:26:21 +0200

    env

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(
         cookie: {
             maxAge: 1000 * 60 * 60 * 24, // 1 day
             httpOnly: true,
-            secure: false,
+            secure:false,
             sameSite: 'lax',
         },
     })
