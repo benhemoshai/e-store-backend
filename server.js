6b268add4364db9eeffb0fd3166a3e0834f04799
Shai <benhemoshai@gmail.com>
2024-11-25 14:17:39 +0200

    added

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: 'https://e-store-frontend-pi.vercel.app/products',
     credentials: true, // Allow cookies to be sent with requests
 }));
 app.use(
