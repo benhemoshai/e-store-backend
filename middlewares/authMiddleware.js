ed497f938c55fae78f056fdf955a92c716fd5958
Shai <benhemoshai@gmail.com>
2024-11-28 00:04:08 +0200

    env

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,10 +1,13 @@
 export function isAdmin(req, res, next) {
     if (req.session?.user?.role === 'admin') {
+      console.log('Admin access granted:', req.session.user); // Log admin details
       next();
     } else {
+      console.log('Access denied. Current session:', req.session);
       res.status(403).json({ message: 'Access denied. Admins only.' });
     }
   }
+  
   
   export function isAuthenticated(req, res, next) {
     if (req.session?.user) {
